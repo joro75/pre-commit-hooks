4700939c42cedc4e639eca584633d06ac348ce95
John de Rooij <joro@dare.nl>
2021-01-07 22:31:28 +0100

    Report the project that was not successfully build

--- pre_commit_hooks/check_successful_msbuild.py
+++ pre_commit_hooks/check_successful_msbuild.py
@@ -1,16 +1,21 @@
 import argparse
 import glob
 import os.path
+import re
 from typing import Optional
 from typing import Sequence
 
 
 def main(argv: Optional[Sequence[str]] = None) -> int:
     parser = argparse.ArgumentParser()
-    parser.add_argument('filenames', nargs='*', help='XML filenames to check.')
+    parser.add_argument('filenames', nargs='*', help='Filenames to check.')
+    parser.add_argument(
+        'buildtype', nargs='*',
+        help='Build types that should be checked.',
+    )
     args = parser.parse_args(argv)
 
-    buildtypes = ['Release']
+    buildtypes = args.buildtype or ['Release']
 
     retval = 0
     for filename in args.filenames:
@@ -21,15 +26,19 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
                     dir, '**', build, '*.tlog', 'unsuccessfulbuild',
                 )
                 files = glob.glob(search, recursive=True)
-                if len(files):
-                    dir = os.path.join('.', dir)
-                    print(
-                        f'Unsuccessfulbuild for {build} is present in: {dir}',
+                for buildfile in files:
+                    m = re.match(
+                        r'.*\\(.*)\.tlog\\unsuccessfulbuild', buildfile, re.I,
                     )
-                    retval = 1
+                    if m:
+                        project = m.group(1)
+                        print(
+                            f'Unsuccessful build for {build} in {project}',
+                        )
+                    retval += 1
             except Exception as exc:
                 print(f'{filename}: Exception raised: {exc}')
-                retval = 1
+                retval += 1
     return retval
 
 
