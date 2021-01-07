6ea8bb301f39ce3db648d55179fbb7eeba171087
John de Rooij <joro@dare.nl>
2021-01-07 21:38:46 +0100

    Perform check on unsuccessfulbuild in the Release build

--- pre_commit_hooks/check_successful_msbuild.py
+++ pre_commit_hooks/check_successful_msbuild.py
@@ -1,4 +1,6 @@
+import os.path
 import argparse
+import glob
 from typing import Optional
 from typing import Sequence
 
@@ -8,14 +10,22 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
     parser.add_argument('filenames', nargs='*', help='XML filenames to check.')
     args = parser.parse_args(argv)
 
+    buildtypes = ['Release']
+
     retval = 0
     for filename in args.filenames:
-        try:
-            with open(filename, 'rb') as xml_file:
-                xml.sax.parse(xml_file, handler)
-        except xml.sax.SAXException as exc:
-            print(f'{filename}: Failed to xml parse ({exc})')
-            retval = 1
+        for build in buildtypes:
+            try:
+                dir = os.path.dirname(filename)
+                search = os.path.join(dir, '**', build, '*.tlog', 'unsuccessfulbuild')
+                files = glob.glob(search, recursive = True)
+                if len(files):
+                    dir = os.path.join('.', dir)
+                    print(f'Unsuccessfulbuild for {build} is present in: {dir}')
+                    retval = 1
+            except Exception as exc:
+                print(f'{filename}: Exception raised: {exc}')
+                retval = 1
     return retval
 
 if __name__ == '__main__':
