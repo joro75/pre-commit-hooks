fb56c06c005826e192432897f66faf5ec5cc5512
John de Rooij <john.de.rooij@gmail.com>
2021-01-17 20:42:55 +0100

    Add last modified time of file compared to *.lastbuildstate file

--- pre_commit_hooks/check_successful_c_msbuild.py
+++ pre_commit_hooks/check_successful_c_msbuild.py
@@ -1,10 +1,18 @@
 """Pre-commit hook to check if the last C MSBuild was successfull."""
 import argparse
+import datetime
 import glob
 import os.path
 import re
 from typing import Optional
 from typing import Sequence
+
+
+def get_file_modified_time(filename: str) -> datetime.datetime:
+    """Determine the file modified time of the passed file."""
+    filestat = os.stat(filename)
+    mtime = datetime.datetime.fromtimestamp(filestat.st_mtime)
+    return mtime
 
 
 def main(argv: Optional[Sequence[str]] = None) -> int:
@@ -20,22 +28,45 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
 
     retval = 0
     for filename in args.filenames:
-        for build in buildtypes:
-            dir = os.path.dirname(filename)
-            search = os.path.join(
-                dir, '**', build, '*.tlog', 'unsuccessfulbuild',
-            )
-            files = glob.glob(search, recursive=True)
-            for buildfile in files:
-                m = re.match(
-                    r'.*\\(.*)\.tlog\\unsuccessfulbuild', buildfile, re.I,
+        fullpath = os.path.join(os.curdir, filename)
+        if os.path.exists(fullpath):
+            file_date = get_file_modified_time(fullpath)
+            for build in buildtypes:
+                dir = os.path.dirname(fullpath)
+                failed_search = os.path.join(
+                    dir, '**', build, '*.tlog', 'unsuccessfulbuild',
                 )
-                if m:
-                    project = m.group(1)
-                    print(
-                        f'Unsuccessful build for {build} in {project}',
+                failed_files = glob.glob(failed_search, recursive=True)
+                for buildfile in failed_files:
+                    m = re.match(
+                        r'.*\\(.*)\.tlog\\unsuccessfulbuild', buildfile, re.I,
                     )
-                retval += 1
+                    if m:
+                        project = m.group(1)
+                        print(
+                            f'Unsuccessful build for {build} in {project}.',
+                        )
+                    retval += 1
+
+                latest_search = os.path.join(
+                    dir, '**', build, '*.tlog', '*.lastbuildstate',
+                )
+                latest_files = glob.glob(latest_search, recursive=True)
+                for latest_file in latest_files:
+                    build_date = get_file_modified_time(latest_file)
+                    if build_date <= file_date:
+                        m = re.match(
+                            r'.*\\(.*)\.tlog\\.*\.lastbuildstate',
+                            latest_file, re.I,
+                        )
+                        if m:
+                            project = m.group(1)
+                            print(
+                                f'Build for {build} in {project} is '
+                                'older then the file.',
+                            )
+                        retval += 1
+
     return retval
 
 
--- tests/check_successful_c_msbuild_test.py
+++ tests/check_successful_c_msbuild_test.py
@@ -1,4 +1,6 @@
 """Unittests for successfull C MSBuild pre-commit hook"""
+import pathlib
+import time
 import unittest
 
 from pre_commit_hooks.check_successful_c_msbuild import main
@@ -23,7 +25,7 @@ class TestSuccessfulCMSBuild_case1(unittest.TestCase):
 
     def test_main_non_existing_file(self):
         self.assertEqual(
-            1, main(('--filenames', 'tests\\case1\\file_not_existing.c')),
+            0, main(('--filenames', 'tests\\case1\\file_not_existing.c')),
         )
 
     def test_main_non_existing_buildtype(self):
@@ -64,6 +66,59 @@ class TestSuccessfulCMSBuild_case2(unittest.TestCase):
             0, main((
                 '--filenames', 'tests\\case2\\file2.c',
                 '--buildtype', 'Debug', '--buildtype', 'Release',
+            )),
+        )
+
+
+class TestSuccessfulCMSBuild_case3(unittest.TestCase):
+    r"""Testcase for testing the successful C MSBuild, where this
+    testcase uses the file in the tests\case3\ subdirectory which
+    reflects an successful build for the 'Release' and the
+    'Debug' buildtype.
+    However the lastbuildstate of the Release is more recent then
+    the file. The lastbuildstate of the Debug is older then the file.
+    """
+
+    @staticmethod
+    def _touch_file(filename: str) -> None:
+        file = pathlib.Path(filename)
+        file.touch(exist_ok=True)
+
+    @classmethod
+    def setUpClass(cls):
+        cls._touch_file(
+            'tests\\case3\\intermediate\\Debug\\project3.tlog\\'
+            'project3.lastbuildstate',
+        )
+        time.sleep(1.2)
+        cls._touch_file('tests\\case3\\file3.c')
+        time.sleep(1.2)
+        cls._touch_file(
+            'tests\\case3\\intermediate\\Release\\project3.tlog\\'
+            'project3.lastbuildstate',
+        )
+
+    def test_main_file_is_newer(self):
+        self.assertEqual(
+            1, main((
+                '--filenames', 'tests\\case3\\file3.c',
+                '--buildtype', 'Debug',
+            )),
+        )
+
+    def test_main_build_is_newer(self):
+        self.assertEqual(
+            0, main((
+                '--filenames', 'tests\\case3\\file3.c',
+                '--buildtype', 'Release',
+            )),
+        )
+
+    def test_main_multiple_build_types(self):
+        self.assertEqual(
+            1, main((
+                '--filenames', 'tests\\case3\\file3.c',
+                '--buildtype', 'Release', '--buildtype', 'Debug',
             )),
         )
 
